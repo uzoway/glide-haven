8aabaea4a69b6ea02e9a7145e1de3b79c4a84bd3
Uzochukwu Okafor <uzochukwuokafor01@gmail.com>
2024-04-26 01:23:11 +0100

    feat(interaction): Add the hover "play video" interaction and setup a mockup of the real scroll video transform interaction for testing purposes.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.querySelector("body").addEventListener("click", () => {
-    document.querySelector(".overlay").classList.toggle("active");
-})
+// document.querySelector("body").addEventListener("click", () => {
+//     document.querySelector(".overlay").classList.toggle("active");
+// })
 
 
 gsap.registerPlugin(CustomEase, Flip);
@@ -9,6 +9,7 @@ CustomEase.create("ease-out-quad", "0.25,0.46,0.45,0.94");
 CustomEase.create("ease-out-quart", "0.165,0.84,0.44,1");
 CustomEase.create("ease-out-cubic", "0.215,0.61,0.355,1");
 CustomEase.create("ease-in-cubic", "0.55,0.055,0.675,0.19");
+CustomEase.create("ease-in-quart", "0.895,0.03,0.685,0.22");
 
 
 function initAnimation() {
@@ -66,3 +67,59 @@ function initAnimation() {
 }
 
 window.addEventListener("DOMContentLoaded", initAnimation);
+
+
+// Play Video button on hover interaction
+const videoItemsContainer = document.querySelector(".video__container--items");
+
+const playVideoButtonTl = gsap.timeline({paused: true});
+
+playVideoButtonTl.set(".video__container .video__container--items .video__container--toolkit", { yPercent: -50, xPercent: -50 })
+                .to(".video__container .video__container--items .video__container--toolkit", { 
+                    scale: 1, 
+                    transformOrigin: "center center", 
+                    duration: 1.7, 
+                    ease: "expo.inOut"
+                })
+                .to(".video__container--toolkit .toolkit__content span", {
+                    y: 0,
+                    stagger: 0.1,
+                    duration: 0.5,
+                    ease: "expo.out"
+                })
+
+
+videoItemsContainer.addEventListener("mouseenter", () => playVideoButtonTl.play())
+
+
+const videosContainer = document.querySelector(".videos");
+
+document.querySelector(".scroll__text").addEventListener("click", () => {
+    let activeVideoContainer = document.querySelector(".video__wrapper:last-child");
+    
+    activeVideoContainer.style.animation = "slide-up 1s cubic-bezier(0.165,0.84,0.44,1) forwards";
+
+    setTimeout(() => {
+        activeVideoContainer.style.animation = "";
+
+        videosContainer.prepend(activeVideoContainer);
+    }, 1000);
+});
+
+
+document.querySelector(".move").addEventListener("click", () => {
+    let lastActiveVideoContainer = document.querySelector(".video__wrapper:first-child");
+
+    gsap.timeline()
+    .set(lastActiveVideoContainer, {
+        yPercent: -100,
+        onComplete: () => {
+            videosContainer.append(lastActiveVideoContainer);
+        }
+    })
+    .to(lastActiveVideoContainer, {
+        yPercent: 0,
+        duration: 1,
+        ease: "ease-out-quart"
+    })
+});
